c986adaf149d79422364ada4207d91f032f3de1e
SebastianBuftea <sebibuftea85@gmail.com>
2024-02-16 16:02:06 +0100

    [ADD]seeder

--- database/migrations/2024_02_16_142214_create_comics_table.php
+++ database/migrations/2024_02_16_142214_create_comics_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -22,7 +23,6 @@ return new class extends Migration
             $table->string('series', 40);
             $table->date('sale_date');
             $table->string('type', 40);
-            $table->string('artists', 100);
             $table->text('writers');
             $table->text('artists');
             $table->timestamps();
