33f37722af2e6b4c99e60aad3e4da1690b63890f
SebastianBuftea <sebibuftea85@gmail.com>
2024-02-19 16:44:50 +0100

    [ADD] forms in create_comic.blade

--- resources/views/comics/comics.blade.php
+++ resources/views/comics/comics.blade.php
@@ -10,13 +10,18 @@
                 <div class="row ">
                     <div class="col-12">
                         <div class="col-12 d-flex justify-content-end">
+                            {{-- creiamo il collegamento alla pagina che conterrà le form di inseriemnto  --}}
                             <a href="{{ route('comics.create') }}" class="m-3">
                                 <button class="btn btn-info ">Add NewComic</button>
                             </a>
                         </div>
                     </div>
+
+                    {{-- creiamo il ciclo che genererà le card di comics --}}
                     @foreach ($comics as $comic)
                         <div class="col-2">
+                            {{-- creiamo un link attraverso cui al click saremoi riamdati alla rotta comic show che 
+                                visulizza la singola card selezionata con i rispettivi dati --}}
                             <a href="{{ route('comics.show', ['comic' => $comic['id']]) }}">
                                 <div class="card my-2 card_style">
                                     <img src="{{ $comic['thumb'] }}" class="comic_img " alt="{{ $comic['title'] }}">
--- resources/views/comics/create_comic.blade.php
+++ resources/views/comics/create_comic.blade.php
@@ -1,15 +1,69 @@
 @extends('layout.app')
 @section('content')
-    <div class="container">
-        <div class="row">
+    <main>
+        <div class="container ">
+            <div class="row">
 
-            <div class="col-12">
-                <h2 class="text-center">Aggiungi un nuovo comic</h2>
-            </div>
-            <div class="col-12">
-                <form action="{{ route('comics.store') }}" method="POST">
-                </form>
+                <div class="col-12 ">
+                    <h2 class="text-center color_white">Add a New Comic</h2>
+                </div>
+                {{-- creiano il collegamento con la rotta comic store che si occupera di salvare i dati inseriti --}}
+                <div class="col-12 color-form my-5">
+                    <form action="{{ route('comics.store') }}" method="POST">
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title" class="my-1">Title</label>
+                            <input type="text" name="title" id="title" class="form-control my-1"
+                                placeholder="Enter the Title of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="description" class="my-1">Description</label>
+                            <input type="text" name="description" id="description" class="form-control my-1"
+                                placeholder="Enter the Description of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="thumb" class="my-1">Thumb</label>
+                            <input type="text" name="thumb" id="thumb" class="form-control my-1"
+                                placeholder="Enter the Thumb of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="price" class="my-1">Price</label>
+                            <input type="text" name="price" id="price" class="form-control my-1"
+                                placeholder="Enter the Price of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="series" class="my-1">Series</label>
+                            <input type="text" name="series" id="series" class="form-control my-1"
+                                placeholder="Enter the Series of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="sale_data" class="my-1">Sale data</label>
+                            <input type="date" name="sale_data" id="sale_data" class="form-control my-1"
+                                placeholder="Enter the Sale data of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="type" class="my-1">Type</label>
+                            <input type="text" name="type" id="type" class="form-control my-1"
+                                placeholder="Enter the Type of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="writers" class="my-1">Writers</label>
+                            <input type="text" name="writers" id="writers" class="form-control my-1"
+                                placeholder="Enter the Writers of the comic" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="artists" class="my-1">Artists</label>
+                            <input type="text" name="artists" id="artists" class="form-control my-1"
+                                placeholder="Enter the Artists of the comic" required>
+                        </div>
+                        <div class="form-group py-3 d-flex justify-content-end">
+                            <button class="btn btn-success" type="submit"> Add</button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    </main>
 @endsection
